4ae4cced3a16212e90e54a1c99ef351859dd81e1
kunalcode12 <kunal818196@gmail.com>
2024-11-10 01:06:02 +0530

    l

--- server.js
+++ server.js
@@ -17,16 +17,19 @@ connectCloudinary();
 // middlewares
 app.use(express.json());
 // app.use(cors());
-app.use(
-  cors({
-    origin: "https://thriftera.vercel.app",
-  })
-);
 // app.use(
 //   cors({
-//     origin: "https://thriftstore-sage.vercel.app/",
+//     origin: "https://thriftera.vercel.app",
 //   })
 // );
+const corsOptions = {
+  origin: "https://thriftera.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Enable if you're using cookies or authentication
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // api endpoints
 app.use("/api/user", userRouter);
@@ -37,5 +40,7 @@ app.use("/api/order", orderRouter);
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+
+app.options("*", cors(corsOptions));
 
 app.listen(port, () => console.log("Server started on PORT : " + port));
